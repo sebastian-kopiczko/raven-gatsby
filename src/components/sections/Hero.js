ea5a01c4fc82cc083ce9ac73dfd5f26941bd8aa9
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-07-10 21:05:26 +0200

    Hero section

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,9 +1,17 @@
 import React from "react"
 import Section from "../Section"
+import PageLink from "../PageLink"
 const Hero = props => {
   return (
-    <Section id="home" className="hero" center={true} uppercase={true}>
-      <p className="hero__content content">{props.content}</p>
+    <Section
+      id="home"
+      className="hero"
+      center={true}
+      uppercase={true}
+      heading="Welcome to Raven"
+    >
+      <p className="hero__content content">responsive multi purpose theme</p>
+      <PageLink to="services" text="read more" />
     </Section>
   )
 }
