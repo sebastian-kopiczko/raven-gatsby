3a94c4c574dd7a7bf6592271a3f4ff55c6283fbe
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-07-10 21:08:01 +0200

    Create Page Link component

--- src/components/PageLink.js
+++ src/components/PageLink.js
@@ -4,11 +4,10 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 const PageLink = props => {
-  let pageLinkClassName = "pageLink"(
-    props.color && (pageLinkClassName += `pageLink--${props.color}`)
-  )
+  let pageLinkClassName = "pageLink"
+  props.color && (pageLinkClassName += ` pageLink--${props.color}`)
   return (
-    <Link to={`#${props.linkTo}`} className={pageLinkClassName}>
+    <Link to={`#${props.to}`} className={pageLinkClassName}>
       {props.text}
     </Link>
   )
