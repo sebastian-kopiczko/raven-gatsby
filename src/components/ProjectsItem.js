523bd75440925b0de2a3213005f283c14d695c5b
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-07-10 21:09:16 +0200

    Work on projects section, create project item component

---
+++ src/components/ProjectsItem.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const ProjectsItem = props => {
+  return (
+    <div className={`projects__image icon--search`}>
+      <img src="" alt={props.imageAlt} />
+    </div>
+  )
+}
+
+export default ProjectsItem
--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,10 +1,38 @@
 import React from "react"
 import Section from "../Section"
+import ProjectsItem from "../ProjectsItem"
+import PageLink from "../PageLink"
 
 const Projects = props => {
+  const projects = [
+    "thumb-1",
+    "thumb-2",
+    "thumb-3",
+    "thumb-4",
+    "thumb-5",
+    "thumb-6",
+    "thumb-7",
+    "thumb-8",
+    "thumb-9",
+    "thumb-10",
+    "thumb-11",
+    "thumb-12",
+  ]
   return (
-    <Section id="projects" className="projects" center={true} uppercase={true}>
-      <h5>Projects</h5>
+    <Section
+      id="projects"
+      className="projects"
+      center={true}
+      uppercase={true}
+      heading="Projects"
+    >
+      <div className="projects__gallery">
+        {projects.map((project, index) => (
+          <ProjectsItem imageAlt={project} />
+        ))}
+      </div>
+      <PageLink text="browse more" to="" color="purple" />
+
       {/* //     <div className="projects__gallery">
   //       {data.allFile.edges.map((img, index) => {
   //         return (
