813edc56f3d2a4244f9509fe8dab644c705a9e99
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-07-10 21:08:48 +0200

    Add services item component

--- src/components/ServicesItem.js
+++ src/components/ServicesItem.js
@@ -1,6 +1,12 @@
 import React from "react"
+import Icon from "./Icon"
 
-const ServicesItem = () => {
-  return <div>single services item</div>
+const ServicesItem = props => {
+  return (
+    <div className="card">
+      <Icon name={props.iconName} />
+      <h4 className="card__content text-uppercase center">{props.text}</h4>
+    </div>
+  )
 }
 export default ServicesItem
