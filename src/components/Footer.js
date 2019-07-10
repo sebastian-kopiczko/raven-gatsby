abf33f83e664648ba72ffc6986b49049337f872d
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-07-10 21:06:02 +0200

    Working with footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,33 @@
 import React from "react"
+import { Link } from "gatsby"
+import Icon from "./Icon"
 
 const Footer = props => {
+  const footerNavItems = [
+    { name: "facebook", link: "https://facebook.com/" },
+    { name: "twitter", link: "https://twitter.com/" },
+    { name: "google", link: "https://google.com/" },
+    { name: "youtube", link: "https://youtube.com/" },
+    { name: "arrow-up", link: "#home" },
+  ]
+  const currentYear = new Date().getFullYear()
   return (
     <footer className="footer">
-      <div className="container">
-        <div className="row">
-          <p className="footer__copyright">{props.copyright}</p>
-          {/* TODO MAP THROUGH SOCIAL ITEMS */}
-          {/* <ul className="footer__navigation">
-              {data.site.siteMetadata.socialNav.map((li, index) => {
-                return (
-                  <li
-                    key={index}
-                    className={`footer__navigation__item icon--${li.icon}`}
-                  >
-                    &nbsp;
-                    <Link to={li.url} className="footer__navigation__link" />
-                  </li>
-                )
-              })}
-            </ul> */}
-        </div>
+      <div className="footer__container">
+        <ul className="footer__navigation">
+          {footerNavItems.map((item, index) => (
+            <li key={index} className="footer__navigation__item">
+              {item === "arrow-up" ? (
+                <Link to={item.link} />
+              ) : (
+                <a href={item.link}>
+                  <Icon name={item.name} />
+                </a>
+              )}
+            </li>
+          ))}
+        </ul>
+        <p className="footer__copyright">Copyright &copy; {currentYear}</p>
       </div>
     </footer>
   )
