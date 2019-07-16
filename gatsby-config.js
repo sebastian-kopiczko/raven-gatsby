5c3799e432e4c9eac469878c19643dfcb9657276
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-07-16 14:48:03 +0200

    Install plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,15 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: "src",
-        path: `${__dirname}/src`,
+        name: "projects",
+        path: "./data",
       },
     },
+    "gatsby-transformer-json",
   ],
 }
