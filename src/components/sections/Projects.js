253522600ec0d27e46ced498a166bf89143d8a7d
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-07-22 14:47:35 +0200

    Add inner and content container, format and clean

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Section from "../Section"
-import ProjectsItem from "../ProjectsItem"
+// import ProjectsItem from "../ProjectsItem"
 import PageLink from "../PageLink"
 
-import ProjectPreview from "../ProjectPreview"
+import ProjectCard from "../ProjectCard"
 
 const Projects = props => {
   const data = useStaticQuery(graphql`
@@ -13,6 +13,7 @@ const Projects = props => {
         edges {
           node {
             title
+            subtitle
             slug
             url
             description
@@ -33,42 +34,50 @@ const Projects = props => {
   return (
     <Section
       id="projects"
-      className="projects"
+      customClassName="projects"
       center={true}
       uppercase={true}
       heading="Projects"
     >
-      <div className="projects__gallery">
-        {projects.map(({ node: project }, index) => {
-          const title = project.title
-          const description = project.description
-          const imageData = project.image.childImageSharp.fluid
-          const slug = project.slug
+      <div className="projects__inner">
+        <div className="projects__content">
+          <p>
+            Laboriosam natus officiis molestias dolorem delectus enim at facilis
+            rerum animi in? Maiores quasi, ut quo sit odio dignissimos illo?
+            Libero amet corrupti atque ipsam, dolore soluta velit, asperiores.
+          </p>{" "}
+          <p>
+            Praesentium molestias rerum accusantium doloribus vero sapiente
+            reiciendis nesciunt laudantium. Voluptate omnis illo nemo
+            asperiores.
+          </p>
+        </div>
+        <div className="projects__gallery">
+          {projects.map(({ node: project }, index) => {
+            const title = project.title
+            const subtitle = project.subtitle
+            const description = project.description
+            const imageData = project.image.childImageSharp.fluid
+            const slug = project.slug
 
-          return (
-            <ProjectPreview
-              title={title}
-              description={description}
-              imageData={imageData}
-              slug={slug}
-            />
-          )
-          // <ProjectsItem imageAlt={project} />
-        })}
+            return (
+              <ProjectCard
+                title={title}
+                subtitle={subtitle}
+                description={description}
+                imageData={imageData}
+                slug={slug}
+              />
+            )
+          })}
+        </div>
       </div>
-      <PageLink text="browse more" to="" color="purple" />
-
-      {/* //     <div className="projects__gallery">
-  //       {data.allFile.edges.map((img, index) => {
-  //         return (
-  //           <Img
-  //             className="projects__image icon--search"
-  //             key={index}
-  //             resolutions={img.node.childImageSharp.fluid}
-  //           />
-  //         )
-  //       })}
-  //     </div></div> */}
+      <PageLink
+        text="browse more"
+        to=""
+        color="purple"
+        customClassName="projects__link"
+      />
     </Section>
   )
 }
