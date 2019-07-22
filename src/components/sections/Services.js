ced81562da0d25f92873a95aa098e5a9e34b7545
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-07-22 14:48:55 +0200

    Query section image, add inner and conent containers, format and clean

--- src/components/sections/Services.js
+++ src/components/sections/Services.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
-
+import Image from "gatsby-image"
 import Section from "../Section"
 
 import ServicesItem from "../ServicesItem"
-import servicesPhoto from "../../images/services-man.png"
+
 const Services = props => {
   const data = useStaticQuery(graphql`
     {
@@ -17,43 +17,66 @@ const Services = props => {
           }
         }
       }
+      file(relativePath: { eq: "images/services-image.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
     }
   `)
 
+  // ...GatsbyImageSharpFluid
   const services = data.allServicesJson.edges
+  const imageData = data.file.childImageSharp.fluid
+
+  const serviceIconClass = "service-item__icon"
   return (
     <Section
       id="services"
-      className="services"
+      heading="Our services"
+      customClassName="services"
       center={true}
       uppercase={true}
-      heading="Our services"
     >
-      <img src={servicesPhoto} alt="" />
-      <p className="content services__content">
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout.
-      </p>
-      {services && (
-        <div className="services__container">
-          <ul className="services__list">
-            {services.map(({ node: service }, index) => {
-              const title = service.title
-              const description = service.description
-              const icon = service.icon_name
-              return (
-                <li key={index} className="services__item">
-                  <ServicesItem
-                    title={title}
-                    iconName={icon}
-                    description={description}
-                  />
-                </li>
-              )
-            })}
-          </ul>
+      <div className="services__inner">
+        <div className="services__content">
+          <p>
+            Corporis explicabo saepe ab a voluptatum accusamus quo, harum
+            laborum placeat similique sequi iure nam aliquid ipsum error omnis
+            atque consequatur eaque perferendis nostrum.
+          </p>
+          <p>
+            Officiis eveniet cupiditate alias iure quis possimus magnam libero
+            obcaecati saepe deleniti neque corporis harum velit delectus odit
+            beatae dignissimos, asperiores voluptas.
+          </p>
         </div>
-      )}
+        <Image className="services__image" fluid={imageData} />
+
+        {services && (
+          <div className="services__container">
+            <ul className="services__list">
+              {services.map(({ node: service }, index) => {
+                const title = service.title
+                const description = service.description
+                const icon = service.icon_name
+                return (
+                  <li key={index} className="services__item">
+                    <ServicesItem
+                      title={title}
+                      iconName={icon}
+                      description={description}
+                      customClassName={serviceIconClass}
+                    />
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
     </Section>
   )
 }
