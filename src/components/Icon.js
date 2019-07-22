1c3d57b70f69fec40ed50f103898487242f2b022
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-07-22 14:38:59 +0200

    Destructure props, added custom class name

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,13 +3,16 @@ import PropTypes from "prop-types"
 
 const Icon = ({ name, customClassName }) => {
   return (
-    <span className={`icon icon--${name} ${customClassName}`} aria-label={name}></span>
+    <span
+      className={`icon icon--${name} ${customClassName}`}
+      aria-label={name}
+    ></span>
   )
 }
 
 Icon.propTypes = {
   name: PropTypes.string.isRequired,
-  customClassName: PropTypes.string
+  customClassName: PropTypes.string,
 }
 
 export default Icon
