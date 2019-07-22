f98e7ecf0dfcdd9e0e54526f1abe9d5788f79eb4
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-07-22 14:44:59 +0200

    Format and clean

--- src/components/TestimonialsItem.js
+++ src/components/TestimonialsItem.js
@@ -1,10 +1,9 @@
 import React from "react"
-import Image from 'gatsby-image'
+import Image from "gatsby-image"
 
 const TestimonialsItem = ({ content, imageAlt, imageData }) => {
   return (
     <div className="testimonials__item swiper-slide">
-      {/* <img src="" alt={imageAlt} className="testimonials__img center" /> */}
       <Image fluid={imageData} alt={imageAlt} />
 
       <p className="testimonials__content">{content}</p>
