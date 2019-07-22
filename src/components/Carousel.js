2450491c2498b72c5dbfacee3ec50377e9f33fd5
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-07-22 14:34:44 +0200

    Remove navigation elements

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import Swiper from 'react-id-swiper';
-import "react-id-swiper/lib/styles/scss/swiper.scss";
+import React from "react"
+import Swiper from "react-id-swiper"
+import "react-id-swiper/lib/styles/scss/swiper.scss"
 
 const Carousel = ({ children }) => {
-    const params = {
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-        }
-    }
-    return (
-        <Swiper {...params}>
-            {children}
-        </Swiper>
-    )
-};
-export default Carousel;
+  const params = {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  }
+  return <Swiper {...params}>{children}</Swiper>
+}
+
+export default Carousel
