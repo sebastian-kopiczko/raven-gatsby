dcd036f2f5165ed4b0caa33dd2b8f960a193026b
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-07-22 14:46:10 +0200

    Import and set background image, changing class names

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,18 +1,22 @@
 import React from "react"
 import Section from "../Section"
 import PageLink from "../PageLink"
+import BackgroundImage from '../../../data/images/bg-home.jpg'
+
 const Hero = props => {
+
   return (
     <Section
       id="home"
-      className="hero"
+      customClassName="hero"
       center={true}
       uppercase={true}
       heading="Welcome to Raven"
+      style={{ backgroundImage: `url(${BackgroundImage})` }}
     >
       <p className="hero__content content">responsive multi purpose theme</p>
-      <PageLink to="services" text="read more" />
-    </Section>
+      <PageLink customClassName="hero__link" to="services" text="read more" />
+    </Section >
   )
 }
 export default Hero
