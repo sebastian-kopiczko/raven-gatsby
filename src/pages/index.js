ce96e43ca9877a8f83da246918aea5078e83c390
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-07-22 14:51:50 +0200

    Clean, add id for main

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,13 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import { Hero, Services, Projects, Testimonials } from "../components/sections/"
 const IndexPage = () => {
-  // query dla tresci sekcji
-
   return (
     <div>
       <Layout>
-        <main>
-          <Hero content="asd" />
+        <main id="main">
+          <Hero />
           <Services />
           <Projects />
           <Testimonials />
